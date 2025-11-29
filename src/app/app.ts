0dac4801ec5759858ec6977d4d4a4faa1359e4e8
Eduard Diumin <edwarddumin@gmail.com>
2025-11-29 17:09:31 +0200

    feat: add user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header';
+import { UserComponent } from './user/user';
+import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, UserComponent, Tasks],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('test-app');
+  users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
+  onSelectUser(id: string) {
+    console.log(`Selected user ID: ${id}`);
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.scss',
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return `assets/users/${this.avatar}`;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
